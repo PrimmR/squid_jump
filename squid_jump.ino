26c1f489da34a10f95730867e91e300017b3e8a3
PrimmR <PrimmR@icloud.com>
2023-08-06 21:17:06 +0100

    Fixed midair freeze bug

--- squid_jump.ino
+++ squid_jump.ino
@@ -366,22 +366,19 @@ const uint8_t PROGMEM Block[] = {
 struct Player {
   float x;
   float y;
-  Rect box;
   float velocity;  // Up is +
   bool falling;
   int charge;
 
-  void setX(float newX) {  // When x accelleration added
-    x = newX;
-    box.x = newX;
+  int intX() {
+    return x;
   }
-  void setY(float newY) {
-    y = newY;
-    box.y = newY;
+  int intY() {
+    return y;
   }
 };
 
-struct Player player = { WIDTH / 2, HEIGHT / 2, Rect(WIDTH / 2, HEIGHT / 2, PLAYER_SIZE, PLAYER_SIZE), 0, true, 0 };
+struct Player player = { WIDTH / 2, HEIGHT / 2, 0, true, 0 };
 
 Rect ground = Rect(0, HEIGHT - 2 * BLOCK_SIZE, WIDTH, BLOCK_SIZE);
 
@@ -404,10 +401,10 @@ void titlescreen() {
 
 void gameinput() {
   if (arduboy.pressed(LEFT_BUTTON) && player.falling) {
-    player.box.x--;
+    player.x--;
   }
   if (arduboy.pressed(RIGHT_BUTTON) && player.falling) {
-    player.box.x++;
+    player.x++;
   }
   if (arduboy.pressed(A_BUTTON)) {
     if (player.charge < MAX_CHARGE) {
@@ -425,19 +422,20 @@ void gameinput() {
   // arduboy.print(player.charge);
   // arduboy.print(" ");
 
-  if (player.box.x < 0) {
-    player.box.x = 0;
-  } else if (player.box.x + PLAYER_SIZE > WIDTH) {
-    player.box.x = WIDTH - PLAYER_SIZE;
+  if (player.x < 0) {
+    player.x = 0;
+  } else if (player.x + PLAYER_SIZE > WIDTH) {
+    player.x = WIDTH - PLAYER_SIZE;
   }
 }
 
-// Calculates whether platform would be hit next frame
+// Calculates whether platform would be hit next frame (may fail if collision boxes both 1px)
 bool collision(Rect platform) {  // Assume that it's always called by player
   if (player.velocity < 0) {
-    Rect ray = Rect(player.box.x, player.box.y + PLAYER_SIZE, player.box.width, -1 * ceil(player.velocity));
-    Rect platformtop = Rect(platform.x, platform.y, platform.width, 1);
-    // arduboy.fillRect(player.box.x, player.box.y + PLAYER_SIZE, player.box.width, -1 * ceil(player.velocity)); // Visual
+    Rect ray = Rect(player.x, player.y + PLAYER_SIZE + camerapos, PLAYER_SIZE, -ceil(player.velocity));
+    Rect platformtop = Rect(platform.x, platform.y + camerapos, platform.width, 2);
+    // arduboy.fillRect(player.x, player.y + PLAYER_SIZE + camerapos, PLAYER_SIZE, -1 * ceil(player.velocity)); // Visual
+    // arduboy.fillRect(platform.x, platform.y + camerapos, platform.width, 1); // Visual
     return Arduboy2::collide(ray, platformtop);
   }
   return false;
@@ -445,14 +443,14 @@ bool collision(Rect platform) {  // Assume that it's always called by player
 
 void physics() {
   if (player.falling) {
-    player.setY(player.box.y - player.velocity);
+    player.y -= player.velocity;
     player.velocity -= 0.1;
   }
   if (collision(ground)) {
-    player.setY(ground.y - PLAYER_SIZE);
+    player.y = ground.y - PLAYER_SIZE;
     player.falling = false;
   } else if (collision(platform)) {
-    player.setY(platform.y - PLAYER_SIZE);
+    player.y = platform.y - PLAYER_SIZE;
     player.falling = false;
   }
   // arduboy.print(player.velocity);
@@ -462,10 +460,10 @@ void physics() {
 void movecamera() {
   // PLayer location on screen player.box.y + camerapos
   // camerapos = (-1 * player.y) + 40;
-  if (player.box.y + camerapos < CAM_UPPER_BOUNDARY) {
-    camerapos = -player.box.y + CAM_UPPER_BOUNDARY;
-  } else if (player.box.y + camerapos > CAM_LOWER_BOUNDARY) {
-    camerapos = -player.box.y + CAM_LOWER_BOUNDARY;
+  if (player.y + camerapos < CAM_UPPER_BOUNDARY) {
+    camerapos = -player.y + CAM_UPPER_BOUNDARY;
+  } else if (player.y + camerapos > CAM_LOWER_BOUNDARY) {
+    camerapos = -player.y + CAM_LOWER_BOUNDARY;
   }
 
   // arduboy.print(camerapos);
@@ -492,7 +490,7 @@ void drawgame() {
 
   
   // Draw player
-  Sprites::drawExternalMask(player.box.x, player.box.y + camerapos, Player, Player_Mask, sprite, sprite);
+  Sprites::drawExternalMask(player.x, player.y + camerapos, Player, Player_Mask, sprite, sprite);
 }
 
 void gameplay() {
