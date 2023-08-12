c37d80c173c0ec5c5268968b514d6ef5ca3ea059
PrimmR <PrimmR@icloud.com>
2023-08-12 20:59:02 +0100

    Split files

---
+++ Sound.h
@@ -0,0 +1,1 @@
+const uint16_t jump_sound[] PROGMEM = { NOTE_D6, 40, NOTE_D5, 40, NOTE_FS6, 40, TONES_END };
---
+++ Structs.h
@@ -0,0 +1,34 @@
+struct Player {
+  double x;
+  double y;
+  float velocity;   // Up is +
+  float xvelocity;  // Right is +
+  bool falling;
+  int charge;
+  int lastplatform;
+
+  int intX() {
+    return floor(x);
+  }
+  int intY() {
+    return floor(y);
+  }
+};
+
+struct Platform {
+  int x;
+  int y;
+  int len;
+  int type;
+};
+
+#define MAX_PLATFORMS 30  // 'Not in the mood' for dynamic lists
+struct Stage {
+  int num;
+  struct Platform platforms[MAX_PLATFORMS];
+  int totalplatforms;
+};
+
+struct Zapfish {
+  int y;
+};
